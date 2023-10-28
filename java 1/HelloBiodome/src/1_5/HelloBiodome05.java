ea99b9a35b8fc7ba77db2f086f2194bef4e4c7bd
secripite <keonho04@gmail.com>
2023-10-28 19:59:26 +0900

    HelloBiodome05 end with bonus task

--- java 1/HelloBiodome/src/1_5/HelloBiodome05.java
+++ java 1/HelloBiodome/src/1_5/HelloBiodome05.java
@@ -3,7 +3,8 @@ public class HelloBiodome05 {
         for (int g = 0; g < 16; g++) {
             for (int h = 0; h < 16; h++) {
                 if ((g & 1 >> g << 2 | h + g ^ h) == 1 && (g % 2 << h >> g | 1 & 0 ^ 0) == 2) {
-                    System.out.printf("h의 값 : %d, g의 값 : %d, (h*h+g)*(h<<h)+(g<<g) = %d",h , g,(h*h+g)*(h<<h)+(g<<g));
+                    System.out.printf("h의 값 : %d, g의 값 : %d, (h*h+g)*(h<<h)+(g<<g) = %d", h , g,(h*h+g)*(h<<h)+(g<<g));
+                    System.out.printf("\n Bonus\nh의 값 : %d, g의 값 : %d, h*h*(h<<h) + g*(h<<h) + (g<<g) = %d", h , g, h*h*(h<<h) + g*(h<<h) + (g<<g));
                 }
             }
         }
