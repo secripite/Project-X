6fe0e5f4e064dd48f787260161a1329adb8c3374
secripite <keonho04@gmail.com>
2023-10-28 15:01:29 +0900

    commit

--- java 1/HelloBiodome/src/1_1/HelloBiodome01.java
+++ java 1/HelloBiodome/src/1_1/HelloBiodome01.java
@@ -5,6 +5,8 @@ public class HelloBiodome01 {
             String name = new String(args[0]);
             name = "";
             name = name_sum(args, name, 0);
+            System.out.printf("안녕하세요 %s님! :)",name);
+            System.out.println("Bonus");
             System.out.printf("안녕하세요 \"%c%c%c%c%c%c%c%c%c%c\"님!", name.charAt(0),name.charAt(1),name.charAt(2),name.charAt(3),name.charAt(4),name.charAt(5),name.charAt(6),name.charAt(7),name.charAt(8),name.charAt(9));
         } catch (Exception e) {
             System.out.print("다시 입력해 주세요");
