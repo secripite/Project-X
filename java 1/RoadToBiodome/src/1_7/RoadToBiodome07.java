f096295993f51e2fee41a64a66625ff236389b95
secripite <keonho04@gmail.com>
2023-11-04 09:53:02 +0900

    readme

---
+++ java 1/RoadToBiodome/src/1_7/RoadToBiodome07.java
@@ -0,0 +1,9 @@
+public class RoadToBiodome07 {
+    public static void main(String[] args) {
+        int[] values = new int[args.length];
+        String[] names = new String[args.length];
+        for (int i = 0; i < args.length; i++) {
+
+        }
+    }
+}
